a317f86bd50c9b46d6e6ef47189dbb3d85dfd242
Jason Chown <j.chown@vtime.net>
2022-01-16 17:43:21 +0000

    Building infrastructure for HA addon

---
+++ OzricDeployTest/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Text.Json;
+
+namespace OzricDeployTest
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+
+            var optionsJson = File.ReadAllText("/data/options.json");
+            var options = JsonSerializer.Deserialize(optionsJson, typeof(Dictionary<string, object>));
+            
+            Console.WriteLine("Options: " + JsonSerializer.Serialize(options));
+        }
+    }
+}
