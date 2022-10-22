fa8de268a531775b999a8b5f9daa6f1be8be1bec
Jason Chown <git@jasonchown.com>
2022-10-22 11:28:02 +0100

    Only ask to build the actually used platforms

--- local_publish.sh
+++ local_publish.sh
@@ -1,3 +1,4 @@
 #!/bin/sh
 
-docker run --name ozric-builder --rm --privileged -v ~/.docker:/root/.docker -v $(pwd):/addon homeassistant/amd64-builder --all -t /addon 
+docker run --name ozric-builder --rm --privileged -v ~/.docker:/root/.docker -v $(pwd):/addon homeassistant/amd64-builder --all -t /addon --test
+ 
